9fe17da51589870339aa3a37e89f9bbe6ceab2a3
ngonza04 <ngonza04@uw.edu>
2019-11-04 16:28:47 -0800

    Add minion GET and POST

--- server/api.js
+++ server/api.js
@@ -1,6 +1,19 @@
-const express = require('express');
+const express = require("express");
 const apiRouter = express.Router();
+const db = require("./db");
 
+apiRouter.get("/minions", (req, res, next) => {
+  const minions = db.getAllFromDatabase("minions");
+  if (minions) {
+    res.send(minions);
+  } else {
+    res.send([]);
+  }
+});
 
+apiRouter.post("/minions", (req, res, next) => {
+  const newMinion = db.addToDatabase("minions", req.body);
+  res.status(201).send(newMinion);
+});
 
 module.exports = apiRouter;
